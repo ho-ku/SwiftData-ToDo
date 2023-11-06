de85c5520bce41d43ab2dbc9f694635beec0e98b
Denys Andriievskyi <denys.an@psoft.pro>
2023-11-06 22:51:16 +0100

    Added CI workflow

--- ToDo-SwiftDataTests/Utilities/DateFormatter+Tests.swift
+++ ToDo-SwiftDataTests/Utilities/DateFormatter+Tests.swift
@@ -9,12 +9,16 @@ import XCTest
 @testable import ToDo_SwiftData
 
 final class DateFormatter_Tests: XCTestCase {
+    
+    private var defaultDate: Date = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd:MM:yyyy HH:mm"
+        return formatter.date(from: "12:12:2024 15:00")!
+    }()
 
     func testCustomFormats() {
-        let date = Date(timeIntervalSince1970: 86400*800)
-        
-        XCTAssertEqual(DateFormatter.dayMonthYear.string(from: date), "11.03.1972")
-        XCTAssertEqual(DateFormatter.hourMinute.string(from: date), "01:00")
+        XCTAssertEqual(DateFormatter.dayMonthYear.string(from: defaultDate), "12.12.2024")
+        XCTAssertEqual(DateFormatter.hourMinute.string(from: defaultDate), "15:00")
     }
 
 }
